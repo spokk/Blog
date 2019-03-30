66ef772b7afbc42cae2364a66a04820a28b0c470
Spok Kirk <game1life1@gmail.com>
2019-03-30 19:27:17 +0200

    add bodyParser

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI;
