151a183cbc01f7f2851acc5e024997a4b19ad675
Spok Kirk <game1life1@gmail.com>
2019-04-03 00:38:23 +0300

    add react-router and test routes + components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import './App.sass';
+
+//test
+import Home from './components/test/Home';
+import About from './components/test/About';
+import Error from './components/test/Error';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>React</p>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <p>React</p>
+            <Link to="/">home link! </Link>
+            <Link to="/about">about link!</Link>
+            <hr />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="" component={Error} />
+            </Switch>
+          </header>
+        </div>
+      </BrowserRouter>
     );
   }
 }
